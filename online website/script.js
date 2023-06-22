065c306fe32c230e513136e6a69ed147a06d2861
040govind <93587527+040govind@users.noreply.github.com>
2023-06-23 00:14:23 +0530

    Delete online website directory

--- online website/script.js
+++
@@ -1,6 +0,0 @@
-function showTextarea() {
-    var selectBox = document.getElementById("jobTitle");
-    var textareaWrapper = document.getElementById("textareaWrapper");
-    textareaWrapper.style.display =
-      selectBox.value === "others" ? "block" : "none";
-  }
